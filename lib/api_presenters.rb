418228c73efe00df57a83bc7cbb114075c6e3207
André Arko & Katlyn Daniluk <pair+andr+katlyn@mavenlink.com>
2012-11-02 12:26:41 -0700

    Add presenter collection singleton

--- lib/api_presenters.rb
+++ lib/api_presenters.rb
@@ -3,4 +3,7 @@ require "api_presenters/base"
 require "api_presenters/presenter_collection"
 
 module ApiPresenters
+  def self.presenter_collection
+    @presenter_collection ||= PresenterCollection.new
+  end
 end
