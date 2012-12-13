939dcb85c6f9d1fda83f97e45abcf94642dd2ce9
Andre Arko <andre@arko.net>
2012-12-13 14:32:05 -0800

    document the ApiPresenter module

--- lib/api_presenter.rb
+++ lib/api_presenter.rb
@@ -3,30 +3,45 @@ require "api_presenter/base"
 require "api_presenter/presenter_collection"
 require "api_presenter/controller_methods"
 
+# The ApiPresenter module itself contains a +default_namespace+ class attribute and a few helpers that make managing +PresenterCollections+ and their corresponding namespaces easier.
 module ApiPresenter
-  extend self
 
-  attr_writer :default_namespace
+  # Sets {default_namespace} to a new value.
+  # @param [String] namespace
+  # @return [String] the new default namespace
+  def self.default_namespace=(namespace)
+    @default_namespace = namespace
+  end
 
+  # The namespace that will be used by {presenter_collection} and {add_presenter_class} if none is given or implied.
+  # @return [String] the default namespace
   def self.default_namespace
     @default_namespace || "none"
   end
 
-  def presenter_collection(namespace = default_namespace)
+  # @param [String] namespace
+  # @return [PresenterCollection] the {PresenterCollection} for the given namespace.
+  def self.presenter_collection(namespace = default_namespace)
     @presenter_collection ||= {}
     @presenter_collection[namespace.to_s.downcase] ||= PresenterCollection.new
   end
 
-  def add_presenter_class(presenter_class, *klasses)
+  # Helper method to quickly add presenter classes that are in a namespace. For example, +add_presenter_class(Api::V1::UserPresenter, "User")+ would add +UserPresenter+ to the PresenterCollection for the +:v1+ namespace as the presenter for the +User+ class.
+  # @param [ApiPresenter::Base] presenter_class The presenter class that is being registered.
+  # @param [Array<String, Class>] klasses Classes that will be presented by the given presenter.
+  def self.add_presenter_class(presenter_class, *klasses)
     presenter_collection(namespace_of(presenter_class)).add_presenter_class(presenter_class, *klasses)
   end
 
-  def namespace_of(klass)
+  # @param [Class] klass The Ruby class whose namespace we would like to know.
+  # @return [String] The name of the module containing the passed-in class.
+  def self.namespace_of(klass)
     names = klass.to_s.split("::")
     names[-2] ? names[-2] : default_namespace
   end
 
-  def logger
+  # @return [Logger] The Rails logger, if it exists, or a STDOUT logger.
+  def self.logger
     @logger ||= begin
       if defined?(Rails)
         Rails.logger
@@ -36,4 +51,5 @@ module ApiPresenter
       end
     end
   end
+
 end
