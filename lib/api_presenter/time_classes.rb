efffe64857c8a2c59ebe8a051f7a2a947d8ce12d
Andre Arko <andre@arko.net>
2012-12-17 17:52:42 -0800

    don't be passive aggressive and bitchy

--- lib/api_presenter/time_classes.rb
+++ lib/api_presenter/time_classes.rb
@@ -2,7 +2,7 @@ module ApiPresenter
   class Base
     # This constant stores an array of classes that we will treat as times.
     # Unfortunately, ActiveSupport::TimeWithZone does not descend from
-    # Time, so we have to try to put it into this array for later use.
+    # Time, so we put them into this array for later use.
     TIME_CLASSES = [Time]
 
     begin
