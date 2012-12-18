f0b0056d69305746cbb9fb39236ffda4e3388c3e
Andre Arko <andre@arko.net>
2012-12-17 18:29:04 -0800

    document the FieldProxy class

--- lib/api_presenter/field_proxy.rb
+++ lib/api_presenter/field_proxy.rb
@@ -1,8 +1,19 @@
 module ApiPresenter
 
+  # FieldProxy acts as a standin for associations and optional fields.
+  # @api private
   class FieldProxy
-    attr_reader :method_name, :optional
+    # @!attribute [r] method_name
+    # @return [Symbol] The name of the method that is being proxied.
+    attr_reader :method_name
 
+    # @!attribute [r] optional
+    # @return [Boolean] Whether this field is optional or not (defaults to +false+)
+    attr_reader :optional
+
+    # @param method_name The name of the method being proxied. Not required if
+    #   a block is passed instead.
+    # @option options [Boolean] :optional Declares the field being proxied to be optional.
     def initialize(method_name = nil, options = {}, &block)
       if block_given?
         raise ArgumentError, "Method name is invalid with a block" if method_name
@@ -15,6 +26,9 @@ module ApiPresenter
       @optional = options[:optional]
     end
 
+    # Call the method or block being proxied.
+    # @param model The object to call the proxied method on.
+    # @return The value returned by calling the method or block being proxied.
     def call(model)
       @block ? @block.call : model.send(@method_name)
     end
