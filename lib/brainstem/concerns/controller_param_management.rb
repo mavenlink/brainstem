b5c7110980bff1c8d7737e6790a782ad80fa69ea
Rob Yurkowski <rob@yurkowski.net>
2015-11-16 10:16:49 -0500

    Add class-level brainstem_model_name.

--- lib/brainstem/concerns/controller_param_management.rb
+++ lib/brainstem/concerns/controller_param_management.rb
@@ -1,21 +1,37 @@
-# Provide `brainstem_model_name` and `brainstem_plural_model_name` in controllers for use when accessing the `params` hash.
+# Provide `brainstem_model_name` and `brainstem_plural_model_name` in
+# controllers for use when accessing the `params` hash.
 
 module Brainstem
   module Concerns
     module ControllerParamManagement
       extend ActiveSupport::Concern
 
-      included do
-        class_attribute :brainstem_plural_model_name, :brainstem_model_name,
-                        instance_accessor: false, instance_reader: false, instance_writer: false
-      end
-
       def brainstem_model_name
-        self.class.brainstem_model_name.to_s.presence || controller_name.singularize
+        self.class.brainstem_model_name.to_s.presence ||
+          controller_name.singularize
       end
 
       def brainstem_plural_model_name
-        self.class.brainstem_plural_model_name.to_s.presence || brainstem_model_name.pluralize
+        self.class.brainstem_plural_model_name.to_s.presence ||
+          brainstem_model_name.pluralize
+      end
+
+      class_methods do
+        def brainstem_model_name
+          @brainstem_model_name ||= controller_name.singularize
+        end
+
+        def brainstem_plural_model_name
+          @brainstem_plural_model_name ||= self.brainstem_model_name.pluralize
+        end
+
+        def brainstem_model_name=(name)
+          @brainstem_model_name = name
+        end
+
+        def brainstem_plural_model_name=(name)
+          @brainstem_plural_model_name = name
+        end
       end
     end
   end
