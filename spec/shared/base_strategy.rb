e1a08279c37f050a7094fbc1054c5eb309af4ba9
mavenlink <mavenlink@mavenlinks-iMac-4.local>
2018-01-02 14:55:33 -0800

    Do better error messages

--- spec/shared/base_strategy.rb
+++ spec/shared/base_strategy.rb
@@ -7,15 +7,15 @@ shared_examples_for Brainstem::QueryStrategies::BaseStrategy do
     let(:result) { strategy.calculate_per_page }
 
     [
-      # per_page  default_per_page  max_per_page  default_max_per_page  expected   message
-      [       10,               20,          100,                  200,       10,  "per page < max"],
-      [      nil,               20,          100,                  200,       20,  "no per page and default < max"],
-      [      nil,              200,          100,                  200,      100,  "no per page and default > max"],
-      [      150,               20,          100,                  200,      100,  "per page > max"],
-      [      150,               20,          nil,                  200,      150,  "no max and per page < default max"],
-      [      250,               20,          nil,                  200,      200,  "no max and per page > default max"],
-    ].each do |per_page, default_per_page, max_per_page, default_max_per_page, expected, message|
-      describe "for '#{message}'" do
+      # per_page  default_per_page  max_per_page  default_max_per_page  expected   situation                             used
+      [       10,               20,          100,                  200,       10,  "per page < max",                     "the per page"],
+      [      nil,               20,          100,                  200,       20,  "no per page and default < max",      "the default"],
+      [      nil,              200,          100,                  200,      100,  "no per page and default > max",      "the max"],
+      [      150,               20,          100,                  200,      100,  "per page > max",                     "the max"],
+      [      150,               20,          nil,                  200,      150,  "no max and per page < default max",  "the per page"],
+      [      250,               20,          nil,                  200,      200,  "no max and per page > default max",  "the default max"],
+    ].each do |per_page, default_per_page, max_per_page, default_max_per_page, expected, situation, used|
+      describe "when #{situation}" do
         let(:options) {
           {
             params: {
@@ -27,7 +27,7 @@ shared_examples_for Brainstem::QueryStrategies::BaseStrategy do
           }
         }
 
-        it "calculates the expected result'" do
+        it "uses #{used}" do
           expect(result).to eq(expected)
         end
       end
