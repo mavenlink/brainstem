0f8a7a4794b8592049dcaa6050d99797b5ef98f9
Shirish Pampoorickal <shirish@mavenlink.com>
2018-02-12 14:15:51 -0800

    Add sink that generates open api specification

---
+++ spec/brainstem/api_docs/sinks/open_api_specification_sink_spec.rb
@@ -0,0 +1,37 @@
+require 'spec_helper'
+require 'brainstem/api_docs/sinks/open_api_specification_sink'
+require 'brainstem/api_docs/formatters/open_api_specification/info_formatter'
+
+module Brainstem
+  module ApiDocs
+    module Sinks
+      describe OpenApiSpecificationSink do
+        let(:write_method)  { Object.new }
+        let(:atlas) { Object.new}
+        let(:options) {
+          {
+            write_method: write_method,
+            write_path: './'
+          }
+        }
+
+        subject { described_class.new(options) }
+
+        it "calls write info" do
+          mock.proxy(subject).write_info_object!
+          mock(subject).write_spec_to_file!
+          mock.proxy.any_instance_of(Brainstem::ApiDocs::Formatters::OpenApiSpecification::InfoFormatter).call
+
+          subject << atlas
+        end
+
+        it "write info properly triggers file write" do
+          mock.proxy(subject).write_spec_to_file!
+          mock(write_method).call('./specification_v2.yml', anything)
+
+          subject << atlas
+        end
+      end
+    end
+  end
+end
