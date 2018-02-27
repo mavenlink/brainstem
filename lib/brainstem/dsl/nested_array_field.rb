32a9ca650c8e6c873c75bdd3838ccfa5bc191c9d
Shirish Pampoorickal <shirish@mavenlink.com>
2018-02-27 10:59:21 -0800

    Add sub fields of array fields to ignore value of parent block and use the evaluated presenter model

--- lib/brainstem/dsl/nested_array_field.rb
+++ lib/brainstem/dsl/nested_array_field.rb
@@ -19,13 +19,22 @@ module Brainstem
 
           configuration.each do |field_name, field|
             if field.presentable?(model, context)
-              model_for_field = field.options[:use_parent_value] ? evaluated_model : model
+              model_for_field = use_parent_value?(field) ? evaluated_model : model
+
               result[field_name] = field.run_on(model_for_field, context, context[:helper_instance])
             end
           end
 
           result
         end
+      end
+
+      private
+
+      def use_parent_value?(field)
+        return true unless field.options.has_key?(:use_parent_value)
+
+        field.options[:use_parent_value]
       end
     end
   end
--- spec/brainstem/dsl/nested_array_field_spec.rb
+++ spec/brainstem/dsl/nested_array_field_spec.rb
@@ -16,15 +16,25 @@ describe Brainstem::DSL::NestedArrayField do
 
     # Add sub fields to the nested array field.
     nested_field.configuration[:name] = Brainstem::DSL::Field.new(:name, type, {})
-    nested_field.configuration[:parent_name] = Brainstem::DSL::Field.new(:parent_name, type, dynamic: -> (model) { model.parent.try(:name) })
+    nested_field.configuration[:parent_name] = Brainstem::DSL::Field.new(:parent_name, type,
+      dynamic: -> (model) { model.parent.try(:name) }
+    )
+    nested_field.configuration[:secret] = Brainstem::DSL::Field.new(:secret, type,
+      via: :secret_info,
+      use_parent_value: false
+    )
 
-    expect(nested_field.configuration.keys).to eq(%w(name parent_name))
+    expect(nested_field.configuration.keys).to eq(%w(name parent_name secret))
   end
 
   describe '#run_on' do
     let(:context)              { { } }
     let(:helper_instance)      { Object.new }
-    let(:presented_field_data) { tasks.map { |task| { 'name' => task.name, 'parent_name' => task.parent.try(:name) } } }
+    let(:presented_field_data) {
+      tasks.map do |task|
+        { 'name' => task.name, 'parent_name' => task.parent.try(:name), 'secret' => model.secret_info }
+      end
+    }
 
     it 'returns an array of hashes with sub nested properties' do
       expect(nested_field.run_on(model, context)).to eq(presented_field_data)
