c85a9e88adca8f15374d2b41f8a01942edfb375e
Shirish <shirish@mavenlink.com>
2019-08-08 14:51:24 -0700

    Update Rails Introspector to be rails 5.x compatible
    - When parsing routes in the RailsIntrospector, we relied on route.constraints to extract the request types for an endpoint. Rails 5.x onwards, this method returns an empty hash
    - The 'verb' method on ActionDispatch::Journey::Route is supported in both Rails 4.x & 5.x. Unfortunately the format of the result varies in different version of rails which was easily fixed by some conditional logic

--- spec/dummy/rails.rb
+++ spec/dummy/rails.rb
@@ -4,34 +4,47 @@ silence_warnings do
   FakeRailsApplication      = Struct.new(:eager_load!, :routes)
   FakeRailsRoutesObject     = Struct.new(:routes)
   FakeRailsRoutePathObject  = Struct.new(:spec)
-  FakeRailsRoute            = Struct.new(:name, :path, :defaults, :constraints)
+  FakeRailsRoute            = Struct.new(:name, :path, :defaults, :verb)
 end
 
 class Rails
   def self.application
     @application ||= begin
+      #
+      # For Rails 4.1 & 4.2, the `verb` method on a route returns a regular expression.
+      #
       route_1 = FakeRailsRoute.new(
         "fake_descendant",
         FakeRailsRoutePathObject.new(spec: '/fake_descendant'),
         { controller: "fake_descendant", action: "show" },
-        { :request_method => /^GET|POST$/ }
+        /^GET|POST$/
       )
 
       route_2 = FakeRailsRoute.new(
         "route_with_no_controller",
         FakeRailsRoutePathObject.new(spec: '/fake_descendant'),
         { },
-        { :request_method => /^PATCH$/ }
+        /^PATCH$/
       )
 
+      #
+      # For Rails 5.x, the `verb` method on a route returns a string.
+      #
       route_3 = FakeRailsRoute.new(
         "route_with_invalid_controller",
         FakeRailsRoutePathObject.new(spec: '/fake_descendant'),
         { controller: "invalid_controller", action: "show" },
-        { :request_method => /^GET$/ }
+        'GET'
       )
 
-      routes = FakeRailsRoutesObject.new([ route_1, route_2, route_3 ])
+      route_4 = FakeRailsRoute.new(
+        "another_fake_descendant",
+        FakeRailsRoutePathObject.new(spec: '/another_fake_descendant'),
+        { controller: "another_fake_descendant", action: "update" },
+        'PUT|PATCH'
+      )
+
+      routes = FakeRailsRoutesObject.new([ route_1, route_2, route_3, route_4 ])
 
       FakeRailsApplication.new(true, routes)
     end
@@ -54,14 +67,14 @@ class FakeApiEngine
         "fake_descendant",
         FakeRailsRoutePathObject.new(spec: '/fake_route_1'),
         { controller: "fake_descendant", action: "show" },
-        { :request_method => /^GET$/ }
+        /^PUT|PATCH$/
       )
 
       route_2 = FakeRailsRoute.new(
         "fake_descendant",
         FakeRailsRoutePathObject.new(spec: '/fake_route_2'),
         { controller: "fake_descendant", action: "show" },
-        { :request_method => /^GET$/ }
+        'PUT|PATCH'
       )
 
       FakeRailsRoutesObject.new([ route_1, route_2 ])
